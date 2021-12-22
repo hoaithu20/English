eff9db269a447a4340b89c3852cd8fba14699dad
hoaithu20 <nguyenhoaithu653@gmail.com>
2021-12-22 22:38:49 +0700

    fix create question

--- src/requests/create-question.request.ts
+++ src/requests/create-question.request.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
-import { IsArray, IsBoolean, IsEmail, IsEnum, IsNumber, IsOptional, IsString } from "class-validator";
+import { IsArray, IsBoolean, IsEnum, IsNumber, IsOptional, IsString } from "class-validator";
 import { Level } from "src/constants/level.enum";
 import { QuestionStatus } from "src/constants/question-status.enum";
 
@@ -25,20 +25,24 @@ export class CreateQuestionRequest {
   title: string;
 
   @ApiProperty()
+  @IsOptional()
   @IsEnum(Level)
   level: Level;
 
   @ApiProperty()
+  @IsOptional()
   @IsEnum(QuestionStatus)
   status: QuestionStatus;
 
   @ApiProperty()
   @IsBoolean()
+  @IsOptional()
   isHidden: boolean;
 
   @ApiProperty()
   @IsNumber()
   totalAnswer: number;
+  
 
   @ApiProperty()
   @IsArray()
