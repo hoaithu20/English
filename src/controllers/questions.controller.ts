cff519b2cd27a185dd7c2928ae3c7db7944ee510
hoaithu20 <nguyenhoaithu653@gmail.com>
2021-12-24 08:51:28 +0700

    fix swagger

--- src/controllers/questions.controller.ts
+++ src/controllers/questions.controller.ts
@@ -2,13 +2,10 @@ import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ApiBody, ApiTags } from '@nestjs/swagger';
 import { CurrUser } from 'src/decoraters/user.decorator';
 import { User } from 'src/repositories/entities/user.entity';
-import { CreatePackageRequest } from 'src/requests/create-package.request';
 import { CreateQuestionRequest } from 'src/requests/create-question.request';
 import { PagingRequest } from 'src/requests/paging.request';
 import { PaginateResult } from 'src/responses/PaginateResult';
 import { JwtAuthGuard } from 'src/security/jwt-auth.guard';
-import { JwtStrategy } from 'src/security/jwt.strategy';
-import { LocalAuthGuard } from 'src/security/local-auth.guard';
 import { QuestionsService } from 'src/services/questions.service';
 
 @ApiTags('/api/question')
