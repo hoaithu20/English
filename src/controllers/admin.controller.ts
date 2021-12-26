292f83dab6cbf3d1b9d5d97a7a85be0ff3ef86a5
Hermione510 <huongvtt17@gmail.com>
2021-12-26 15:41:41 +0700

    add admin approve

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiTags } from '@nestjs/swagger';
+import { UserRole } from 'src/constants/user-role.enum';
+import { Roles } from 'src/decoraters/role.decorator';
+import { ApproveQuestionRequest } from 'src/requests/approve-question.request';
+import { JwtAuthGuard } from 'src/security/jwt-auth.guard';
+import { AdminService } from 'src/services/admin.service';
 
-@Controller('admin')
-export class AdminController {}
+@ApiTags('/api/auth')
+@Controller('api/admin')
+@UseGuards(JwtAuthGuard)
+@ApiBearerAuth()
+@Roles(UserRole.ADMIN)
+export class AdminController {
+    constructor( private readonly adminService: AdminService) {}
+
+    @ApiBody({
+        type: ApproveQuestionRequest
+    })
+    @Post('approve')
+    async approveQuestion(request: ApproveQuestionRequest) {
+        return await this.adminService.approveQuestion(request)
+    }
+}
